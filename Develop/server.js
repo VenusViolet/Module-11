e21bcb5637e7c5f879dba688ce62777315776a98
Venus Rodin <VenusViolet1234@gmail.com>
2023-03-02 17:39:22 -1000

    trying to fix cloning issue

---
+++ Develop/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+//
+const homeRoutes = require("./routes/homeroutes");
+const apiRoutes = require("./routes/apiRoutes");
+
+const app = express();
+
+//connects to the spefic port we will be using 
+const PORT = process.env.PORT || 3000;
+
+// Built in express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Custome middleware including routes
+app.use("/api", apiRoutes);
+app.use("/", homeRoutes);
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
